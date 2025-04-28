a779b74dea9bb31dea1d9d70bba749bbc8e364a2
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-04-28 15:25:44 +0100

    Create and test /api/articles/:article_id endpoint and edit endpoints.json

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const { getApi, badUrl } = require("./controllers/api.controller");
 const { getTopics } = require("./controllers/topics.controller");
+const { getArticleById } = require("./controllers/articles.controller");
 
 app.use(express.json());
 
@@ -10,13 +11,34 @@ app.get("/api", getApi);
 
 app.get("/api/topics", getTopics);
 
+app.get("/api/articles/:article_id", getArticleById);
+
 app.all("/*url", badUrl);
 //End of express chain
 
 //Start of error handling middleware chain
+
+//404 custom errorhandler
+app.use((err, req, res, next) => {
+  if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg });
+  } else {
+    next(err);
+  }
+});
+
+//400 sql errorhandler
+app.use((err, req, res, next) => {
+  if (err.code === "22P02") {
+    res.status(400).send({ msg: "Bad Request!" });
+  } else {
+    next(err);
+  }
+});
+
 // 500 handler
 app.use((err, req, res, next) => {
-  console.log(err);
+  console.log(err, "<--in 500 Handler");
   res.status(500).send({ msg: "Internal Server Error!" });
 });
 
