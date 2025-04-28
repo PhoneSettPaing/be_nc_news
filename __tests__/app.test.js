3898686cc59f19b80314fd2cec2c2b8abd1ee49e
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-04-28 12:11:44 +0100

    create and test get /api, all /*url endpoints then edit endpoints.json

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,29 @@
 const endpointsJson = require("../endpoints.json");
 /* Set up your test imports here */
+const db = require("../db/connection");
+const app = require("../app");
+const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const data = require("../db/data/test-data/index");
 
 /* Set up your beforeEach & afterAll functions here */
+beforeEach(() => seed(data));
+
+afterAll(() => db.end());
+
+describe("Bad URL errors", () => {
+  test("404: Responds with Not Found! msg", () => {
+    return request(app)
+      .get("/NotAValidUrl")
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Not Found!");
+      });
+  });
+});
 
 describe("GET /api", () => {
-  test.skip("200: Responds with an object detailing the documentation for each endpoint", () => {
+  test("200: Responds with an object detailing the documentation for each endpoint", () => {
     return request(app)
       .get("/api")
       .expect(200)
