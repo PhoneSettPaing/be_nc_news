878b7390ca52c1e26e29fdbae40b04361b18ee3e
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-04-30 11:29:32 +0100

    Create and test patch /api/articles/:article_id endpoint and add 3 new tests to previous task per feedback

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -230,4 +230,152 @@ describe("POST /api/articles/:article_id/comments", () => {
         expect(msg).toBe("article_id Not Found!");
       });
   });
+
+  test("400: Respond with Bad Request!! msg when trying to add comment without empty object in post object", () => {
+    const postObj = {};
+
+    return request(app)
+      .post("/api/articles/5/comments")
+      .send(postObj)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request!!");
+      });
+  });
+
+  test("400: Respond with Bad Request!! msg when trying to add comment without username object in post object", () => {
+    const postObj = {
+      body: "Greate Article",
+    };
+
+    return request(app)
+      .post("/api/articles/3/comments")
+      .send(postObj)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request!!");
+      });
+  });
+
+  test("400: Respond with Bad Request!! msg when trying to add comment without body object in post object", () => {
+    const postObj = {
+      username: "lurker",
+    };
+
+    return request(app)
+      .post("/api/articles/6/comments")
+      .send(postObj)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request!!");
+      });
+  });
+});
+
+describe("Patch /api/articles/:article_id", () => {
+  describe("Positive votes", () => {
+    test("200: Respond with an updated article object with updated votes for given article_id", () => {
+      const patchObj = {
+        inc_votes: 5,
+      };
+
+      return request(app)
+        .patch("/api/articles/4")
+        .send(patchObj)
+        .expect(200)
+        .then(({ body: { article } }) => {
+          expect(article).toMatchObject({
+            article_id: 4,
+            title: "Student SUES Mitch!",
+            topic: "mitch",
+            author: "rogersop",
+            body: "We all love Mitch and his wonderful, unique typing style. However, the volume of his typing has ALLEGEDLY burst another students eardrums, and they are now suing for damages",
+            created_at: "2020-05-06T01:14:00.000Z",
+            votes: 5,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          });
+        });
+    });
+  });
+
+  describe("Negative votes", () => {
+    test("200: Respond with an updated article object with updated votes for given article_id", () => {
+      const patchObj = {
+        inc_votes: -15,
+      };
+
+      return request(app)
+        .patch("/api/articles/6")
+        .send(patchObj)
+        .expect(200)
+        .then(({ body: { article } }) => {
+          expect(article).toMatchObject({
+            article_id: 6,
+            title: "A",
+            topic: "mitch",
+            author: "icellusedkars",
+            body: "Delicious tin of cat food",
+            created_at: "2020-10-18T01:00:00.000Z",
+            votes: -15,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          });
+        });
+    });
+  });
+
+  test("400: Respond with Bad Request! msg when trying to update article with votes that is not a number", () => {
+    const patchObj = {
+      inc_votes: "Not A Number",
+    };
+
+    return request(app)
+      .patch("/api/articles/1")
+      .send(patchObj)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request!");
+      });
+  });
+
+  test("400: Respond with Bad Request! msg when trying to update article with valid votes but unvalid article_id ", () => {
+    const patchObj = {
+      inc_votes: 10,
+    };
+
+    return request(app)
+      .patch("/api/articles/NotArticleId")
+      .send(patchObj)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request!");
+      });
+  });
+
+  test("404: Respond with article_id Not Found! msg when trying to update article with an article_id that's out of range", () => {
+    const patchObj = {
+      inc_votes: 2,
+    };
+
+    return request(app)
+      .patch("/api/articles/1000")
+      .send(patchObj)
+      .expect(404)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("article_id Not Found!");
+      });
+  });
+
+  test("400: Respond with Bad Request!! msg when trying to update article with empty patch object", () => {
+    const patchObj = {};
+
+    return request(app)
+      .patch("/api/articles/3")
+      .send(patchObj)
+      .expect(400)
+      .then(({ body: { msg } }) => {
+        expect(msg).toBe("Bad Request!!");
+      });
+  });
 });
--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -1,6 +1,7 @@
 const {
   selectArticleById,
   selectArticles,
+  updateArticleById,
 } = require("../models/articles.model");
 
 exports.getArticleById = (req, res, next) => {
@@ -16,4 +17,19 @@ exports.getArticles = (req, res) => {
   return selectArticles().then((articles) => {
     res.status(200).send({ articles });
   });
+};
+
+exports.patchArticleById = (req, res, next) => {
+  const { article_id } = req.params;
+  const { inc_votes } = req.body;
+
+  if (!inc_votes) {
+    return Promise.reject({ status: 400, msg: "Bad Request!!" });
+  }
+
+  return updateArticleById(article_id, inc_votes)
+    .then((article) => {
+      res.status(200).send({ article });
+    })
+    .catch(next);
 };
