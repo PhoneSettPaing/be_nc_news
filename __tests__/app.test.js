8b51e6aea54fe49270f8d524b0240057600ddace
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-04-30 13:48:27 +0100

    Create and test delete /api/comments/:comment_id end point

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -380,7 +380,7 @@ describe("PATCH /api/articles/:article_id", () => {
   });
 });
 
-describe.only("DELETE /api/comments/:comment_id", () => {
+describe("DELETE /api/comments/:comment_id", () => {
   test("204: When comment deleted successfully", () => {
     return request(app).delete("/api/comments/2").expect(204);
   });
