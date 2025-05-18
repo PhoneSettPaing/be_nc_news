109dd867621b584a7c7ebba57e312370481b2505
Phone Sett Paing <mr.phonesettpaing@gmail.com>
2025-05-18 19:41:54 +0100

    Change one test for create_at to expect any string

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -598,7 +598,7 @@ describe("GET /api/articles", () => {
                 title: "Sony Vaio; or, The Laptop",
                 topic: "mitch",
                 author: "icellusedkars",
-                created_at: "2020-10-16T05:03:00.000Z",
+                created_at: expect.any(String),
                 votes: 0,
                 article_img_url:
                   "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
